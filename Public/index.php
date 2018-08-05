9253edae18c510aeaa5ffd0376092a83190fa6cb
G@SeS <very.nice@ukr.net>
2018-08-05 13:49:55 +0300

    End Lesson02

--- Public/index.php
+++ Public/index.php
@@ -1,3 +1,29 @@
 <?php
 
-echo '<h1>Hello, World!</h1>';
+class Room
+{
+    private $color = 'red';
+    public $variable = 'something';
+
+    public function getColor()
+    {
+        echo $this->color;
+    }
+
+    public function setColor($color)
+    {
+        echo $this->color = $color;
+    }
+
+    public function getVariable()
+    {
+        echo $this->variable;
+    }
+}
+
+$room = new Room();
+
+$room->getColor();
+echo '<br>';
+$room->getVariable();
+
