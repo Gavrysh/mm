251a86ed35505d4a237633e70c6d8f637a917222
G@SeS <very.nice@ukr.net>
2018-08-05 14:29:08 +0300

    End lesson03

--- Public/index.php
+++ Public/index.php
@@ -2,28 +2,51 @@
 
 class Room
 {
+    /**
+     * @var string
+     */
     private $color = 'red';
-    public $variable = 'something';
 
+    /**
+     * @var int
+     */
+    protected $windows = 4;
+
+    /**
+     * @var bool
+     */
+    public $lamp = true;
+
+    /**
+     *
+     */
     public function getColor()
     {
         echo $this->color;
     }
 
+    /**
+     * @param $color
+     */
     public function setColor($color)
     {
         echo $this->color = $color;
-    }
-
-    public function getVariable()
-    {
-        echo $this->variable;
     }
 }
 
 $room = new Room();
 
-$room->getColor();
+$room->lamp;
+echo '<h4>Print public property</h4>';
+echo $room->lamp;
 echo '<br>';
-$room->getVariable();
 
+echo 'Now light is ';
+echo $room->lamp ? 'on' : 'off';
+echo '<br>';
+
+//echo $room->windows; // 'Error. Will not work. Protected property.'
+
+//echo $room->color; // 'Error. Will not work. Private property.'
+
+echo '<h3>End of script</h3>';
