9672404c80fd4ae9cace8b98c6e46aa90959ea8c
G@SeS <very.nice@ukr.net>
2018-08-08 17:27:12 +0300

    end lesson15

--- Public/index.php
+++ Public/index.php
@@ -4,24 +4,8 @@ ini_set('display_errors', 1);
 
 chdir(dirname(__DIR__));
 
-//require_once './Src/Gss/Core/Application.php';
-//require_once './App/App.php';
-
-/*function myAutoload($class)
-{
-    $array = explode('\\', $class);
-    switch ($array[0]) {
-        case 'App':
-            require_once './'.implode(DIRECTORY_SEPARATOR, $array).'.php';
-            break;
-        case 'Gss':
-            require_once './Src/'.implode(DIRECTORY_SEPARATOR, $array).'.php';
-            break;
-    }
-}
-
-spl_autoload_register('myAutoload');*/
-
 require_once './vendor/autoload.php';
 
-$obj = new \Gss\App\App();
+(new Gss\Core\ErrorHandler())->register();
+
+require_once './App/errors.php';
